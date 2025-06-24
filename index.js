86575a355681186be761c786fc8b87cf1c30e9a2
baribeop <barib4u2000@yahoo.com>
2025-06-24 01:42:58 +0100

    add error message for invalid route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen( PORT, ()=> {
     
 } catch (error) {
 
-    console.error("server connection fail", error.mesaage)
+    console.error("server connection fail", error.message)
     
 }
 
@@ -159,3 +159,6 @@ app.put("/items/:id", async(req, res)=>{
     }
 })
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route not found' });
+});
